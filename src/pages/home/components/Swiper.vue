88d2d8550fc0d989f7d2bb3fe715995852475744
myth_yu <yumingsheng1990@outlook.com>
2019-07-09 01:37:31 +0800

    swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: '//source.qunarzz.com/site/images/wns/20190626_dujia_homepage_top_banner_5.jpg'
+      }, {
+        id: '002',
+        imgUrl: '//source.qunarzz.com/site/images/wns/20190702_dujia_homepage_top_banner_4.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 25.6%
+    .swiper-img
+      width 100%
+</style>
