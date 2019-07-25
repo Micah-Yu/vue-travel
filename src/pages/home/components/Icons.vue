dea3d37a28df066487378ea348cf9559d05bef6f
myth_yu <yumingsheng1990@outlook.com>
2019-07-25 21:28:34 +0800

    add icon

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,123 @@
+<template>
+    <!--<div class="icons">-->
+      <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(page ,index) of pages" :key="index">
+          <div class="icons">
+            <div class="icon" v-for="item of page" :key="item.id">
+              <div class="icon-img">
+                <img class="icon-img-content" :src="item.imgUrl" alt="pic">
+                <p class="icon-desc">{{item.desc}}</p>
+              </div>
+            </div>
+          </div>
+        </swiper-slide>
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    <!--</div>-->
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      },
+      iconList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票景点门票景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/3e/86314b2af03b7502.png',
+        desc: '水上乐园'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '亲子乐园'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+        desc: '动植物园'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/528a9e80403b8c02.png',
+        desc: '暑期钜惠'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '哈尔滨必游'
+      }, {
+        id: '007',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/ac9ef584416467d8cc557aad650959fd.png',
+        desc: '公园'
+      }, {
+        id: '008',
+        imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
+        desc: '自然风光'
+      }, {
+        id: '009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+        desc: '城市风光'
+      }, {
+        id: '010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/fa/2548667cb6e902.png',
+        desc: '展馆展览'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .icon
+      position relative
+      overflow hidden
+      float left
+      height 0
+      width 25%
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        padding .1rem
+        box-sizing border-box
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+        .icon-desc
+          left 0
+          right 0
+          bottom 0
+          height .44rem
+          line-height .44rem
+          text-align center
+          color $darkTextColor
+          ellipse()
+</style>
